315b845289b35f78db69450f844d49566cd2e0d5
ahmedshahabaz <shahabaz@usf.edu>
2026-03-25 02:38:47 -0400

    audit

---
+++ other_MODELS/DistanceEstimationTracking_AUDIT/torchsparse/torchsparse/nn/functional/voxelize.py
@@ -0,0 +1,26 @@
+import torchsparse_backend
+from torch.autograd import Function
+from torchsparse.nn.functional.hash import *
+
+__all__ = ['spvoxelize']
+
+
+class VoxelizeGPU(Function):
+    @staticmethod
+    def forward(ctx, feat, idx, cnt):
+        out = torchsparse_backend.insertion_forward(feat.float().contiguous(),
+                                                    idx.int().contiguous(),
+                                                    cnt)
+        ctx.for_backwards = (idx.int().contiguous(), cnt, feat.shape[0])
+        return out
+
+    @staticmethod
+    def backward(ctx, top_grad):
+        idx, cnt, N = ctx.for_backwards
+        bottom_grad = torchsparse_backend.insertion_backward(
+            top_grad.float().contiguous(), idx, cnt, N)
+        return bottom_grad, None, None
+
+
+def spvoxelize(feat, idx, cnt):
+    return VoxelizeGPU.apply(feat, idx, cnt)
